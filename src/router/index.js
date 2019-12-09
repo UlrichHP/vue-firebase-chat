a8b8f14164aec79bb0d35c480a9eb4843cae6564
UlrichHP <ulrichhuet@hotmail.com>
2019-12-09 22:54:59 +0100

    feat: bootstrap-vue and login page

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Chat from '../components/Chat.vue'
+import Room from '../components/Room.vue'
+import AddRoom from '../components/AddRoom.vue'
+import Login from '../components/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/chat/:nickname/:roomid/:roomname',
+    name: 'Chat',
+    component: Chat
+  },
+  {
+    path: '/room/:nickname',
+    name: 'RoomList',
+    component: Room
+  },
+  {
+    path: '/add-room',
+    name: 'AddRoom',
+    component: AddRoom
+  },
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
